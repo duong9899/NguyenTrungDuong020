f54434c984761e3ee967915a4c25442d8eee8e78
NGUYEN TRUNG DUONG <1721050202@student.humg.edu.vn>
2021-12-17 15:31:29 +0700

    Firt_commit_NTD202

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NguyenTrungDuong202.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
